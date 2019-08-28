46a2904ae097ad6ecd86eb7900963bcbd3cb5691
regan <reganshrestha499@gmail.com>
2019-08-28 17:55:11 +0545

    part 7

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,11 @@ from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
     def __str__(self):
         return self.question_text
     question_text = models.CharField("The Question",max_length=200)
